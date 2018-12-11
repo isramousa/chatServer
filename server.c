fa9223d082faa01df07594ddf753a02f8100f2fe
Emma Paris <eparis@atlassian.com>
2018-12-11 10:14:31 +0200

    add main server that connect with multiple clients and send the message back

---
+++ server.c
@@ -0,0 +1,178 @@
+#include <stdio.h> 
+#include <string.h> //strlen 
+#include <stdlib.h> 
+#include <errno.h> 
+#include <unistd.h> //close 
+#include <arpa/inet.h> //close 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros 
+	
+#define TRUE 1 
+#define FALSE 0 
+#define PORT 8080 
+	
+int main(int argc , char *argv[]) 
+{ 
+	int opt = TRUE; 
+	int master_socket , addrlen , new_socket , client_socket[30] , 
+		max_clients = 30 , activity, i , valread , sd; 
+	int max_sd; 
+	struct sockaddr_in address; 
+		
+	char buffer[1025]; 
+		
+	
+	fd_set readfds; 
+		
+	
+	char *message = "WELCOME FROM SERVER\n"; 
+	
+	//initialise all client_socket[] to 0 so not checked 
+	for (i = 0; i < max_clients; i++) 
+	{ 
+		client_socket[i] = 0; 
+	} 
+		
+	//create a master socket 
+	if( (master_socket = socket(AF_INET , SOCK_STREAM , 0)) == 0) 
+	{ 
+		perror("socket failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+	
+	
+	if( setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, 
+		sizeof(opt)) < 0 ) 
+	{ 
+		perror("setsockopt"); 
+		exit(EXIT_FAILURE); 
+	} 
+	
+	
+	address.sin_family = AF_INET; 
+	address.sin_addr.s_addr = INADDR_ANY; 
+	address.sin_port = htons( PORT ); 
+		
+	
+	if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0) 
+	{ 
+		perror("bind failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+	printf("Listener on port %d \n", PORT); 
+		
+	
+	if (listen(master_socket, 3) < 0) 
+	{ 
+		perror("listen"); 
+		exit(EXIT_FAILURE); 
+	} 
+		
+	//accept the incoming connection 
+	addrlen = sizeof(address); 
+	puts("Waiting for connections ..."); 
+		
+	while(TRUE) 
+	{ 
+		//clear the socket set 
+		FD_ZERO(&readfds); 
+	
+		//add master socket to set 
+		FD_SET(master_socket, &readfds); 
+		max_sd = master_socket; 
+			
+		//add child sockets to set 
+		for ( i = 0 ; i < max_clients ; i++) 
+		{ 
+			//socket descriptor 
+			sd = client_socket[i]; 
+				
+			//if valid socket descriptor then add to read list 
+			if(sd > 0) 
+				FD_SET( sd , &readfds); 
+				
+			//highest file descriptor number, need it for the select function 
+			if(sd > max_sd) 
+				max_sd = sd; 
+		} 
+	
+
+		activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL); 
+	
+		if ((activity < 0) && (errno!=EINTR)) 
+		{ 
+			printf("select error"); 
+		} 
+			
+		//If something happened on the master socket , 
+		//then its an incoming connection 
+		if (FD_ISSET(master_socket, &readfds)) 
+		{ 
+			if ((new_socket = accept(master_socket, 
+					(struct sockaddr *)&address, (socklen_t*)&addrlen))<0) 
+			{ 
+				perror("accept"); 
+				exit(EXIT_FAILURE); 
+			} 
+			
+			//inform user of socket number - used in send and receive commands 
+			printf("New connection , socket fd is %d , ip is : %s , port : %d \n" , new_socket , inet_ntoa(address.sin_addr) , ntohs (address.sin_port)); 
+				
+			puts("Welcome message sent successfully"); 
+				
+			//add new socket to array of sockets 
+			for (i = 0; i < max_clients; i++) 
+			{ 
+				//if position is empty 
+				if( client_socket[i] == 0 ) 
+				{ 
+					client_socket[i] = new_socket; 
+					printf("Adding to list of sockets as %d\n" , i); 
+						
+					break; 
+				} 
+			} 
+		} 
+			
+		//else its some IO operation on some other socket 
+		for (i = 0; i < max_clients; i++) 
+		{ 
+			sd = client_socket[i]; 
+				
+			if (FD_ISSET( sd , &readfds)) 
+			{ 
+		
+				if ((valread = read( sd , buffer, 1024)) == 0) 
+				{ 
+					//Somebody disconnected , get his details and print 
+					getpeername(sd , (struct sockaddr*)&address ,(socklen_t*)&addrlen); 
+					printf("Host disconnected , ip %s , port %d \n" , 
+						inet_ntoa(address.sin_addr) , ntohs(address.sin_port)); 
+						
+					//Close the socket and mark as 0 in list for reuse 
+					close( sd ); 
+					client_socket[i] = 0; 
+				} 
+					
+				//Echo back the message that came in 
+				else
+				{ 
+
+					
+					buffer[valread] = '\0'; 
+					for(i=0; i<max_clients;i++){
+						if(sd != client_socket[i]){
+							send(client_socket[i] , buffer , strlen(buffer) , 0 );
+						}
+					}
+				        memset(buffer,0 ,1025);
+				} 
+			} 
+		} 
+	} 
+		
+	return 0; 
+} 
+
