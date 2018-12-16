73f487cf7e0efffe179207adfb42f3ca62d67ed8
Isra Mousa <isram@mellanox.com>
2018-12-16 14:18:55 +0200

    refactoring the code

--- client.c
+++ client.c
@@ -7,17 +7,15 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
  
-#define MAXRCVLEN 500
 #define PORT 8080
 
 
 
 
-//aaaaaaaaaaa 
+ 
 int main(int argc, char *argv[])
 {
-   char buffer[MAXRCVLEN + 1]; /* +1 so we can add null terminator */
-   int  mysocket,choice,num,numbers;
+   int  mysocket,choice,num;
    struct sockaddr_in dest; 
    char messageSent[1024];
    char messageRecd[1024];
@@ -44,7 +42,6 @@ int main(int argc, char *argv[])
 	{
 		case 1:
 			printf("enter the message\n");
-			//fgets(messageSent,sizeof(messageSent),stdin);
 			scanf("%s", messageSent);
 			if((send(mysocket,messageSent,strlen(messageSent),0))==-1) {
 				printf("Failure sending message!\n");
@@ -65,47 +62,6 @@ int main(int argc, char *argv[])
 			break;
 	}	
 }
-    /*while(1){
-	printf("Enter the number to make an operation\n");
-        printf("1. send data to server\n");
-        printf("2. close the connection\n");
-
-        scanf("%d",&choice);
-	switch(choice)
-	{
-		case  1 :
-		    while(1){
-			printf("enter the message\n");
-			scanf("%s", messageSent);
-			//fgets(message,sizeof(message),stdin);
-			if((send(mysocket,messageSent,strlen(messageSent),0))==-1) {
-				printf("Failure sending message!\n");
-				close(mysocket);
-				exit(1);
-			}
-			else {
-				printf("message send from client: %s \n",messageSent);
-				num = recv(mysocket,messageRecd, sizeof(messageRecd),0);
-				if( num <= 0)
-				{
-					printf("error in connection\n");
-					break;
-				}
-				messageRecd[num]='\0';
-				printf("message recived from server: %s\n",messageRecd);
-
-			}
-		   }
-			break;		
-		
-		case  2 :
-		default: 
-			close(mysocket);
-			exit(0);
-			break;
-
- }
-}*/
 
    return EXIT_SUCCESS;
 }
