decbcf9e462e643a67ea6e6e1982bfe4a46c6bce
Isra Mousa <isram@mellanox.com>
2018-12-17 20:31:21 +0200

    threads to send and recive data

--- client.c
+++ client.c
@@ -14,17 +14,27 @@
 #define LENGTH 15
 #define MAX_LEN 1024
 
+
+struct argsSend {
+	char name[LENGTH];
+	int socketNumber;
+};
+
+
 void* sendDataThread(void* args)
 {
 	char messageSent[MAX_LEN];
-	int mysocket = *((int *)args);
+	char message[MAX_LEN - 24];
+	char *clientName = ((struct argsSend*)args)->name;
+	int mysocket = ((struct argsSend*)args)->socketNumber;
 	while(1){
-	printf("inside send thread\n");
-	
-	printf("send %d = \n",mysocket);
+	messageSent[0]='\0';
+	message[0]='\0';
+	printf("Enter the msg you want to send:\n");
+	scanf("%s", message);
+	strcat(messageSent,clientName);
+	strcat(messageSent,message);
 
-		scanf("%s", messageSent);
-		//strcat with name of send user
 		if((send(mysocket,messageSent,strlen(messageSent),0))==-1) {
 			printf("Failure sending message!\n");
 			close(mysocket);
@@ -36,45 +46,42 @@ void* sendDataThread(void* args)
 
 void* recvDataThread(void* args)
 {
-	char messageRecd[MAX_LEN];
+        char messageRecd[MAX_LEN];
 	int mysocket = *((int *)args);
 	while(1){
-	printf("inside recive thread\n");
-	printf("rcv %d = \n",mysocket);
 		if(recv(mysocket,messageRecd, sizeof(messageRecd),0) < 0 ){
 			printf("error in connection\n");
 			break;
 		}
-		printf("message recived from server: %s\n",messageRecd);
+		printf("message recived from server:\n %s",messageRecd);
 	}
 
 }
 
+   
 
  
 int main(int argc, char *argv[])
 {
    pthread_t rcvThread, sndThread;
    int  mysocket, rcvThStatus, sndThStatus;
-   int *socketArg = malloc(sizeof(*socketArg));
    struct sockaddr_in dest; 
-   
-   struct argsSend {
-	char name[LENGTH];
-	int socketNumber;
-   }client;
-   
+
+   int *socketArg = malloc(sizeof(*socketArg));
+   struct argsSend *client = (struct argsSend *)malloc(sizeof(struct argsSend));   
 
    mysocket = socket(AF_INET, SOCK_STREAM, 0);
-
+   	
    //fill the info of client need to send
    printf("please enter your name:\n");
-   scanf("%s",&client.name);
-   size_t cur_len = strlen(client.name);
+   scanf("%s",&client->name);
+   client->socketNumber = mysocket;
+   // add : to name
+   size_t cur_len = strlen(client->name);
 	if(cur_len < LENGTH -2) {
-		client.name[cur_len] = ':';
+		client->name[cur_len] = ':';
 	}
-   client.socketNumber = mysocket;
+  
 
    *socketArg = mysocket;	
    memset(&dest, 0, sizeof(dest));                /* zero the struct */
@@ -84,9 +91,9 @@ int main(int argc, char *argv[])
 	
    connect(mysocket, (struct sockaddr *)&dest, sizeof(struct sockaddr_in));
    pthread_create(&rcvThread,NULL,&recvDataThread,(void *)socketArg);
-   pthread_create(&sndThread,NULL,&sendDataThread,(void *)socketArg);
-
-
+   pthread_create(&sndThread,NULL,&sendDataThread,(void *)client);
+   pthread_join(rcvThread,NULL);
+   pthread_join(sndThread,NULL);
 
    return EXIT_SUCCESS;
 }
